fd11558a6ea0349660c3d2cf59fcd945767a2e0d
IrfanCSE <irfanulhasan1996@gmail.com>
2021-03-28 11:19:53 +0600

    swagger set

---
+++ BloodDonorTracker/Extensions/SwaggerExtension.cs
@@ -0,0 +1,47 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace BloodDonorTracker.Extensions
+{
+    public static class SwaggerExtension
+    {
+        public static void AddSwaggerGenService(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(swagger =>
+            {
+                //This is to generate the Default UI of Swagger Documentation  
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Blood Donor Tracker API",
+                    Description = "ASP.NET Core 3.1 Web API"
+                });
+                // To Enable authorization using Swagger (JWT)  
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
+
+                    }
+                });
+            });
+        }
+    }
+}
--- BloodDonorTracker/Startup.cs
+++ BloodDonorTracker/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace BloodDonorTracker
 {
@@ -33,8 +34,8 @@ namespace BloodDonorTracker
             services.DependencyInjection();
             services.AddIdentityServices(_configuration);
 
-            // Register the Swagger generator
-            services.AddSwaggerGen();
+            // Register the Swagger generator : Default : services.AddSwaggerGen()
+            services.AddSwaggerGenService();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
